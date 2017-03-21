7b9815b6006e0545ceb72c90aed478d0ff829d8c
Merritt Adkins <merritt.adkins@gmail.com>
2017-03-20 14:22:56 -1000

    Initial Commit

---
+++ Task 1.6 Retry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task 1.6 Retry
+//
+//  Created by PotPie on 3/20/17.
+//  Copyright © 2017 PotPie - CareerFoundry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
